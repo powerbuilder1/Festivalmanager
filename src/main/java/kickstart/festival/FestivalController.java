3b67d55db73821cb5210daa184ecef75f8a507bb
Conrad Carl <coheca0815@gmail.com>
2021-11-22 00:27:49 +0100

    added simple festivals route

---
+++ src/main/java/kickstart/festival/FestivalController.java
@@ -0,0 +1,25 @@
+package kickstart.festival;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.util.Assert;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class FestivalController {
+
+    FestivalManagement festivalManagement;
+
+    FestivalController(FestivalManagement festivalManagement) {
+        Assert.notNull(festivalManagement, "festivalManagement must not be null");
+        this.festivalManagement = festivalManagement;
+    }
+
+    @GetMapping("/festivals")
+    String festivals(Model model) {
+        model.addAttribute("festivals", festivalManagement.findAllFestivals());
+        model.addAttribute("title", "Festivals");
+        return "festivals";
+    }
+
+}
