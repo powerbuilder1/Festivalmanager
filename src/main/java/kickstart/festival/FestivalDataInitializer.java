daf257c2b4f77a6117af70aba291fcd2c5210b23
Conrad Carl <coheca0815@gmail.com>
2021-11-22 00:18:00 +0100

    festival data initializer class

---
+++ src/main/java/kickstart/festival/FestivalDataInitializer.java
@@ -0,0 +1,19 @@
+package kickstart.festival;
+
+import org.salespointframework.core.DataInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+
+@Order(20)
+@Component
+public class FestivalDataInitializer implements DataInitializer {
+
+    private final Logger LOG = LoggerFactory.getLogger(FestivalDataInitializer.class);
+
+    @Override
+    public void initialize() {
+        LOG.info("Initializing data for {}", getClass().getSimpleName());
+    }
+}
