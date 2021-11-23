77c6745e7ca8d894c725d3c0ce8a659640d1c61d
Conrad Carl <coheca0815@gmail.com>
2021-11-23 21:39:15 +0100

    location repository

---
+++ src/main/java/kickstart/location/LocationRepository.java
@@ -0,0 +1,11 @@
+package kickstart.location;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.util.Streamable;
+
+public interface LocationRepository extends CrudRepository<Location, Long> {
+
+    @Override
+    Streamable<Location> findAll();
+
+}
