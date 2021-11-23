771630b5348890ddcc5dc89f74fd413bf91695d6
powerbuilder1 <philipp.120201@gmail.com>
2021-11-23 16:32:29 +0100

    add Entity Annotation to Food and create constructor with super constructor

--- src/main/java/kickstart/catering/Food.java
+++ src/main/java/kickstart/catering/Food.java
@@ -1,6 +1,17 @@
 package kickstart.catering;
 
+import org.javamoney.moneta.Money;
 import org.salespointframework.catalog.Product;
 
+import javax.persistence.Entity;
+
+@Entity
 public class Food extends Product {
+	public Food(String name, Money price) {
+		super(name, price);
+	}
+
+	public Food() {
+
+	}
 }
