ae9177e861e4406b4ebaf70ebfec37a134be5a93
Conrad Carl <coheca0815@gmail.com>
2021-11-23 23:57:03 +0100

    added route to see all locations

--- src/main/java/kickstart/location/LocationController.java
+++ src/main/java/kickstart/location/LocationController.java
@@ -1,7 +1,10 @@
 package kickstart.location;
 
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.util.Assert;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class LocationController {
@@ -11,6 +14,13 @@ public class LocationController {
     public LocationController(LocationManagement locationManagement) {
         Assert.notNull(locationManagement, "locationManagement must not be null");
         this.locationManagement = locationManagement;
+    }
+
+    @PreAuthorize("hasRolle('Planung')")
+    @GetMapping("/locations")
+    String locations(Model model) {
+        model.addAttribute("locations", locationManagement.findAllLocations());
+        return "locations";
     }
 
 }
