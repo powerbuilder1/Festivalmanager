ad14784ace970a3d74986a8bd236f8086f6fdc9e
Conrad Carl <coheca0815@gmail.com>
2021-11-24 14:57:22 +0100

    added routes for editing a location

--- src/main/java/festivalmanager/location/LocationController.java
+++ src/main/java/festivalmanager/location/LocationController.java
@@ -5,6 +5,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.util.Assert;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 public class LocationController {
@@ -21,6 +24,30 @@ public class LocationController {
     String locations(Model model) {
         model.addAttribute("locations", locationManagement.findAllLocations());
         return "locations";
+    }
+
+    @GetMapping("/location/{id}")
+    String location(@PathVariable long id, Model model, RedirectAttributes redirectAttributes) {
+        Location location = locationManagement.findById(id);
+        if (location == null) {
+            redirectAttributes.addFlashAttribute("error", "LOCATION_NOT_FOUND");
+            return "redirect:/locations";
+        }
+
+        model.addAttribute("location", location);
+        return "location";
+    }
+
+    @GetMapping("/location/{id}/edit/image")
+    String editImage(@PathVariable long id, Model model, RedirectAttributes redirectAttributes) {
+        Location location = locationManagement.findById(id);
+        if (location == null) {
+            redirectAttributes.addFlashAttribute("error", "LOCATION_NOT_FOUND");
+            return "redirect:/locations";
+        }
+
+        model.addAttribute("location", location);
+        return "location_edit_image";
     }
 
 }
