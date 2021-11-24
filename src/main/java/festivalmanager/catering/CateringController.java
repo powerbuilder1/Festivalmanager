4b38524f746d6565edb61562c94042eb5aa37e97
powerbuilder1 <philipp.120201@gmail.com>
2021-11-24 16:55:55 +0100

    implement addItemToCatalog method in CateringController

--- src/main/java/festivalmanager/catering/CateringController.java
+++ src/main/java/festivalmanager/catering/CateringController.java
@@ -1,6 +1,12 @@
 package festivalmanager.catering;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import javax.validation.Valid;
 
 @Controller
 public class CateringController {
@@ -10,5 +16,12 @@ public class CateringController {
 	public CateringController(CateringManagement cateringManagement) {
 		this.cateringManagement = cateringManagement;
 	}
+
+	@PostMapping(path = "catering/addToFoodCatalog")
+	public String addItemToCatalog(@Valid NewFoodItemForm foodItemForm, Errors errors) {
+		cateringManagement.addItemToCatalog(foodItemForm);
+		return "redirect:/";
+	}
+
 
 }
