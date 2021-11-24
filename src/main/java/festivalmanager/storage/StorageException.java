9504ecbec7cb3580de542902c93f33f76e760c0a
Conrad Carl <coheca0815@gmail.com>
2021-11-24 14:56:27 +0100

    added StorageException and StorageProperies

---
+++ src/main/java/festivalmanager/storage/StorageException.java
@@ -0,0 +1,12 @@
+package festivalmanager.storage;
+
+public class StorageException extends RuntimeException {
+
+    public StorageException(String message) {
+        super(message);
+    }
+
+    public StorageException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
