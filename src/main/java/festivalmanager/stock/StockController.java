84625c61ce769a4e13dfb7a24014f9f5e4da2cc6
powerbuilder1 <philipp.120201@gmail.com>
2021-11-25 14:22:19 +0100

    implement deleteAllInventoryItems method in StockController

--- src/main/java/festivalmanager/stock/StockController.java
+++ src/main/java/festivalmanager/stock/StockController.java
@@ -4,6 +4,8 @@ import org.salespointframework.inventory.UniqueInventoryItem;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class StockController {
@@ -24,4 +26,17 @@ public class StockController {
 		model.addAttribute("stock", stockManagment.getCurrentStock());
 		return "stock";
 	}
+
+	@PostMapping(path = "stock/deleteAllInventoryItems/{inventoryItem}")
+	public String deleteAllInventoryItems(@PathVariable UniqueInventoryItem inventoryItem) {
+		stockManagment.deleteAllInventoryItems(inventoryItem);
+		return "stock";
+	}
+	 //TEST
+	@PostMapping(path = "stock/test/deleteAll")
+	public String deleteAll() {
+		stockManagment.deleteAll();
+		return "stock";
+	}
+
 }
