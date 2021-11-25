b2375ccc5c196b716afff4231ad40067b56f1d51
powerbuilder1 <philipp.120201@gmail.com>
2021-11-25 14:23:12 +0100

    implement deleteAllInventoryItems method in StockManagment / implement findByProduct method in StockManagment

--- src/main/java/festivalmanager/stock/StockManagment.java
+++ src/main/java/festivalmanager/stock/StockManagment.java
@@ -1,9 +1,12 @@
 package festivalmanager.stock;
 
+import festivalmanager.catering.Food;
 import org.salespointframework.inventory.UniqueInventory;
 import org.salespointframework.inventory.UniqueInventoryItem;
 import org.springframework.data.util.Streamable;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class StockManagment {
@@ -16,5 +19,18 @@ public class StockManagment {
 
 	public Streamable<UniqueInventoryItem> getCurrentStock() {
 		return inventory.findAll();
+	}
+
+	public void deleteAllInventoryItems(UniqueInventoryItem inventoryItem) {
+		inventory.delete(inventoryItem);
+	}
+
+	public Optional<UniqueInventoryItem> findByProduct(Food foodItem) {
+		return inventory.findByProduct(foodItem);
+	}
+
+	// Test
+	public void deleteAll() {
+		inventory.deleteAll();
 	}
 }
