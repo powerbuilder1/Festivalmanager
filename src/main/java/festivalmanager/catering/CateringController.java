1db477bcf18dcbbc54b18ddcf5040df1316e5502
powerbuilder1 <philipp.120201@gmail.com>
2021-11-25 14:21:03 +0100

    implement deleteItemFromCatalog method in CateringController

--- src/main/java/festivalmanager/catering/CateringController.java
+++ src/main/java/festivalmanager/catering/CateringController.java
@@ -1,13 +1,11 @@
 package festivalmanager.catering;
 
+import org.salespointframework.catalog.ProductIdentifier;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.Errors;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 
@@ -38,5 +36,10 @@ public class CateringController {
 		return "catalog";
 	}
 
+	@PostMapping(path = "catering/deleteFromCatalog/{foodItem}")
+	public String deleteItemFromCatalog(@PathVariable("foodItem") Food foodItem) {
+		cateringManagement.deleteItemFromCatalog(foodItem);
+		return "catalog";
+	}
 
 }
