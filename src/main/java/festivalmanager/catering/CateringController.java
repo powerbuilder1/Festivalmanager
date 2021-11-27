4a5643503cdf71f0283fcd7d68ae12b65dcd3ed8
powerbuilder1 <philipp.120201@gmail.com>
2021-11-27 18:18:08 +0100

    add route for catalog item edit page (getEditCatalogItem()) / add comments

--- src/main/java/festivalmanager/catering/CateringController.java
+++ src/main/java/festivalmanager/catering/CateringController.java
@@ -18,24 +18,40 @@ public class CateringController {
 		this.cateringManagement = cateringManagement;
 	}
 
+	// route to catering management page
 	@GetMapping(path = "catering/management")
 	public String getCateringManagement(Model model) {
 		model.addAttribute("foodItemForm", new NewFoodItemForm());
 		return "catering_management";
 	}
 
-	@PostMapping(path = "catering/addToFoodCatalog")
-	public String addItemToCatalog(@ModelAttribute NewFoodItemForm foodItemForm) {
-		cateringManagement.addItemToCatalog(foodItemForm);
-		return "redirect:/";
-	}
-
+	// route to catalog overview
 	@GetMapping(path = "catering/catalog")
 	public String getCatalog(Model model) {
 		model.addAttribute("catalog", cateringManagement.getCatalog());
 		return "catalog";
 	}
 
+	// route to edit page for catalog items
+	@GetMapping(path = "catering/catalog/edit/{foodItem}")
+	public String getEditCatalogItem(
+			@PathVariable("foodItem") Food foodItem,
+			Model model
+			) {
+		model.addAttribute("foodItem", foodItem);
+		System.out.println(foodItem.getName());
+		System.out.println(foodItem.getPrice());
+		return "catalog_edit_item";
+	}
+
+	// add item to FoodCatalog
+	@PostMapping(path = "catering/addToFoodCatalog")
+	public String addItemToCatalog(@ModelAttribute NewFoodItemForm foodItemForm) {
+		cateringManagement.addItemToCatalog(foodItemForm);
+		return "redirect:/";
+	}
+
+	// delete item from FoodCatalog
 	@PostMapping(path = "catering/deleteFromCatalog/{foodItem}")
 	public String deleteItemFromCatalog(@PathVariable("foodItem") Food foodItem) {
 		cateringManagement.deleteItemFromCatalog(foodItem);
