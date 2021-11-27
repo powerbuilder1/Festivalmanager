7ee93027e2e5a8f0925a653e2e4edf0c2cfc2207
powerbuilder1 <philipp.120201@gmail.com>
2021-11-27 19:33:55 +0100

    add some comments and implement initializeInventoryItem() method in StockManagment

--- src/main/java/festivalmanager/stock/StockManagment.java
+++ src/main/java/festivalmanager/stock/StockManagment.java
@@ -3,6 +3,7 @@ package festivalmanager.stock;
 import festivalmanager.catering.Food;
 import org.salespointframework.inventory.UniqueInventory;
 import org.salespointframework.inventory.UniqueInventoryItem;
+import org.salespointframework.quantity.Quantity;
 import org.springframework.data.util.Streamable;
 import org.springframework.stereotype.Service;
 
@@ -17,17 +18,27 @@ public class StockManagment {
 		this.inventory = inventory;
 	}
 
+	// get current stock
 	public Streamable<UniqueInventoryItem> getCurrentStock() {
 		return inventory.findAll();
 	}
 
+	// delete all inventory items for a specific FoodItem
 	public void deleteAllInventoryItems(UniqueInventoryItem inventoryItem) {
 		inventory.delete(inventoryItem);
 	}
 
+	// find a UniqueInventoryItem by Food-Product
 	public Optional<UniqueInventoryItem> findByProduct(Food foodItem) {
 		return inventory.findByProduct(foodItem);
 	}
+
+	// initialize new Inventory Item for a specific Food-Product
+	public void initializeInventoryItem(Food foodItem, double amount) {
+		inventory.save(new UniqueInventoryItem(foodItem, Quantity.of(amount)));
+	}
+
+
 
 	// Test
 	public void deleteAll() {
