2b457ca1577bf6302f0bc7cf52d856d209816d29
powerbuilder <philipp.120201@gmail.com>
2021-11-28 15:20:23 +0100

    create ReorderForm class template

---
+++ src/main/java/festivalmanager/stock/ReorderForm.java
@@ -0,0 +1,39 @@
+package festivalmanager.stock;
+
+import org.salespointframework.catalog.ProductIdentifier;
+import org.springframework.validation.Errors;
+
+import javax.validation.constraints.NotEmpty;
+import java.io.Serializable;
+
+public class ReorderForm implements Serializable {
+	private @NotEmpty ProductIdentifier foodItemId;
+	private @NotEmpty int amount;
+
+	public ReorderForm(ProductIdentifier foodItemId, int amount) {
+		this.foodItemId = foodItemId;
+		this.amount = amount;
+	}
+
+	public ReorderForm() {
+	}
+
+	public ProductIdentifier getFoodItemId() {
+		return foodItemId;
+	}
+
+	public void setFoodItemId(ProductIdentifier foodItemId) {
+		this.foodItemId = foodItemId;
+	}
+
+	public int getAmount() {
+		return amount;
+	}
+
+	public void setAmount(int amount) {
+		this.amount = amount;
+	}
+
+	public void validate(Errors errors) {
+	}
+}
