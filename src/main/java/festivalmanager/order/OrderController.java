d03d901064bfd93df51526c3e826ac2bbe1848f1
powerbuilder1 <philipp.120201@gmail.com>
2021-12-03 19:57:05 +0100

    add get route for order terminal

--- src/main/java/festivalmanager/order/OrderController.java
+++ src/main/java/festivalmanager/order/OrderController.java
@@ -4,9 +4,13 @@ import org.salespointframework.order.Cart;
 import org.salespointframework.order.Order;
 import org.salespointframework.order.OrderManagement;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.SessionAttributes;
 
+@Controller
 @PreAuthorize("isAuthenticated()")
 @SessionAttributes("cart")
 public class OrderController {
@@ -21,6 +25,11 @@ public class OrderController {
 	@ModelAttribute("cart")
 	Cart initializeCart() {
 		return new Cart();
+	}
+
+	@GetMapping(path = "catering/order")
+	String getCart() {
+		return "catering";
 	}
 
 }
