0e9314bc37950b4b54d1a9715f1eac3865d2b742
powerbuilder1 <philipp.120201@gmail.com>
2021-12-04 00:03:28 +0100

    add model to getCard method in OrderController and implement addFoodToCard method in OrderController

--- src/main/java/festivalmanager/order/OrderController.java
+++ src/main/java/festivalmanager/order/OrderController.java
@@ -1,14 +1,15 @@
 package festivalmanager.order;
 
+import festivalmanager.catering.CateringManagement;
+import festivalmanager.catering.Food;
 import org.salespointframework.order.Cart;
 import org.salespointframework.order.Order;
 import org.salespointframework.order.OrderManagement;
+import org.salespointframework.quantity.Quantity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @PreAuthorize("isAuthenticated()")
@@ -16,9 +17,11 @@ import org.springframework.web.bind.annotation.SessionAttributes;
 public class OrderController {
 
 	private final OrderManagement<Order> orderOrderManagement;
+	private final CateringManagement cateringManagement;
 
-	public OrderController(OrderManagement<Order> orderOrderManagement) {
+	public OrderController(OrderManagement<Order> orderOrderManagement, CateringManagement cateringManagement) {
 		this.orderOrderManagement = orderOrderManagement;
+		this.cateringManagement = cateringManagement;
 	}
 
 
@@ -28,8 +31,21 @@ public class OrderController {
 	}
 
 	@GetMapping(path = "catering/order")
-	String getCart() {
+	String getCart(Model model) {
+		model.addAttribute("catalog", cateringManagement.getCatalog());
 		return "catering";
+	}
+
+	@PostMapping(path = "catering/order")
+	String addFoodToCard(
+			@RequestParam("foodItemId") Food food,
+			@RequestParam("amount") int amount,
+			@ModelAttribute Cart cart
+			) {
+
+		// add food items to card
+		cart.addOrUpdateItem(food, Quantity.of(amount));
+		return "redirect:/catering/order";
 	}
 
 }
