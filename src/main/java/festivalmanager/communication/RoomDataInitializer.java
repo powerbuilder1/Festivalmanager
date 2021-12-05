e81e7e44a597e1f8769052ac6c83cce2e9631099
Conrad Carl <coheca0815@gmail.com>
2021-12-05 19:28:51 +0100

    added room datainitzializer

---
+++ src/main/java/festivalmanager/communication/RoomDataInitializer.java
@@ -0,0 +1,26 @@
+package festivalmanager.communication;
+
+import java.util.List;
+
+import org.salespointframework.core.DataInitializer;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+import org.springframework.util.Assert;
+
+@Order(50)
+@Component
+public class RoomDataInitializer implements DataInitializer {
+
+    private final CommunicationManagement communicationManagement;
+
+    public RoomDataInitializer(CommunicationManagement communicationManagement) {
+        Assert.notNull(communicationManagement, "communicationManagement must not be null!");
+        this.communicationManagement = communicationManagement;
+    }
+
+    @Override
+    public void initialize() {
+        List.of("Catering", "All").forEach(communicationManagement::createRoom);
+    }
+
+}
