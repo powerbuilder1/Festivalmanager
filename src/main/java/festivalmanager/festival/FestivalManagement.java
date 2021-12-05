3035f30f10746313ccc3d2865edf3ca3b6cbbf01
Conrad Carl <coheca0815@gmail.com>
2021-12-05 15:55:45 +0100

    added find all published festivals + comments

--- src/main/java/festivalmanager/festival/FestivalManagement.java
+++ src/main/java/festivalmanager/festival/FestivalManagement.java
@@ -15,6 +15,12 @@ public class FestivalManagement {
     private final FestivalRepository festivalRepository;
     private final LocationManagement locationManagement;
 
+    /**
+     * Constructor
+     * 
+     * @param festivalRepository
+     * @param locationManagement
+     */
     FestivalManagement(FestivalRepository festivalRepository, LocationManagement locationManagement) {
         Assert.notNull(festivalRepository, "festivalRepository must not be null");
         Assert.notNull(locationManagement, "locationManagement must not be null");
@@ -22,6 +28,12 @@ public class FestivalManagement {
         this.locationManagement = locationManagement;
     }
 
+    /**
+     * Create a new festival
+     * 
+     * @param festival
+     * @return
+     */
     public Festival createFestival(Festival festival) {
         Assert.notNull(festival, "festival must not be null");
         if (festival.getLocation() == null) {
@@ -30,29 +42,95 @@ public class FestivalManagement {
         return festivalRepository.save(festival);
     }
 
+    /**
+     * Create a new festival with parameters
+     * 
+     * @param name
+     * @param location
+     * @param beginDate
+     * @param endDate
+     * @param information
+     * @return
+     */
     public Festival createFestival(String name, Location location, String beginDate, String endDate,
             String information) {
         Festival festival = new Festival(name, location, beginDate, endDate, information);
         return createFestival(festival);
     }
 
+    /**
+     * Create a new festival with parameters
+     * 
+     * @param name
+     * @param location
+     * @param beginDate
+     * @param endDate
+     * @param information
+     * @param isPublished
+     * @return
+     */
+    public Festival createFestival(String name, Location location, String beginDate, String endDate,
+            String information, boolean isPublished) {
+        Festival festival = new Festival(name, location, beginDate, endDate, information);
+        festival.setIsPublished(isPublished);
+        return createFestival(festival);
+    }
+
+    /**
+     * Returns all festivals
+     * 
+     * @return
+     */
     public Streamable<Festival> findAllFestivals() {
         return festivalRepository.findAll();
     }
 
+    /**
+     * Returns all published festivals
+     * 
+     * @return
+     */
+    public Streamable<Festival> findAllPublishedFestivals() {
+        return festivalRepository.findAll().filter(festival -> festival.getIsPublished());
+    }
+
+    /**
+     * Returns the festival with the given identifier
+     * 
+     * @param id
+     * @return
+     */
     public Festival findById(long id) {
         return festivalRepository.findById(id).orElse(null);
     }
 
+    /**
+     * Returns all festivals with the given name
+     * 
+     * @param name
+     * @return
+     */
     public Streamable<Festival> findAllByName(String name) {
         return festivalRepository.findAll().filter(festival -> festival.getName().equals(name));
     }
 
+    /**
+     * removes the festival with the given identifier
+     * 
+     * @param id
+     * @return
+     */
     public boolean deleteById(long id) {
         festivalRepository.deleteById(id);
         return true;
     }
 
+    /**
+     * Updates the festival
+     * 
+     * @param festival
+     * @return
+     */
     public Festival updateFestival(Festival festival) {
         Assert.notNull(festival, "festival must not be null");
         if (festival.getLocation() == null) {
