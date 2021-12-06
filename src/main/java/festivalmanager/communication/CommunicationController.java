3b7e33e278d79d3ff1e3d9d15e2a8f4fbbae0f3f
Conrad Carl <coheca0815@gmail.com>
2021-12-06 16:56:59 +0100

    get current user + route to chat

--- src/main/java/festivalmanager/communication/CommunicationController.java
+++ src/main/java/festivalmanager/communication/CommunicationController.java
@@ -2,18 +2,32 @@ package festivalmanager.communication;
 
 import com.mysema.commons.lang.Assert;
 
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import festivalmanager.authentication.User;
+import festivalmanager.authentication.UserManagement;
 
 @Controller
 public class CommunicationController {
 
     private final CommunicationManagement communicationManagement;
+    private final UserManagement userManagement;
 
-    public CommunicationController(CommunicationManagement communicationManagement) {
+    public CommunicationController(CommunicationManagement communicationManagement, UserManagement userManagement) {
         Assert.notNull(communicationManagement, "communicationManagement must not be null");
+        Assert.notNull(userManagement, "userManagement must not be null");
         this.communicationManagement = communicationManagement;
+        this.userManagement = userManagement;
+    }
+
+    public User getCurrentUser() {
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        return userManagement.findByName(auth.getName());
     }
 
     @GetMapping("/news")
@@ -21,6 +35,17 @@ public class CommunicationController {
         model.addAttribute("news", communicationManagement.findAllMessagesInRoom("public"));
         model.addAttribute("titel", "News");
         return "news";
+    }
+
+    @PreAuthorize("hasAnyRole('PLANNING', 'CATERING', 'BOSS')")
+    @GetMapping("/chat")
+    String chat(Model model) {
+        User currentUser = getCurrentUser();
+        if (currentUser == null) {
+            System.out.println("/chat: User is null");
+            return "redirect:/login";
+        }
+        return "chat";
     }
 
 }
