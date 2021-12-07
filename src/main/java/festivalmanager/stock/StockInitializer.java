e192f33159dcc44fa219ff01c103fd29dee029cb
powerbuilder1 <philipp.120201@gmail.com>
2021-12-07 23:52:13 +0100

    add festival relation to FoodInventoryItems in StockInitializer

--- src/main/java/festivalmanager/stock/StockInitializer.java
+++ src/main/java/festivalmanager/stock/StockInitializer.java
@@ -1,5 +1,6 @@
 package festivalmanager.stock;
 
+import festivalmanager.festival.FestivalManagement;
 import org.salespointframework.core.DataInitializer;
 import org.salespointframework.inventory.UniqueInventory;
 import org.salespointframework.inventory.UniqueInventoryItem;
@@ -15,17 +16,20 @@ public class StockInitializer implements DataInitializer {
 
 	private final UniqueInventory<UniqueInventoryItem> inventory;
 	private final FoodCatalog foodCatalog;
+	private final FestivalManagement festivalManagement;
 
-	public StockInitializer(UniqueInventory<UniqueInventoryItem> inventory, FoodCatalog foodCatalog) {
+	public StockInitializer(UniqueInventory<UniqueInventoryItem> inventory, FoodCatalog foodCatalog, FestivalManagement festivalManagement) {
 		this.inventory = inventory;
 		this.foodCatalog = foodCatalog;
+		this.festivalManagement = festivalManagement;
 	}
 
 	@Override
 	public void initialize() {
 		foodCatalog.findAll().forEach(food -> {
-			if (inventory.findByProduct(food).isEmpty()) {
-				inventory.save(new UniqueInventoryItem(food, Quantity.of(5)));
+			if (inventory.findByProduct(food).isEmpty() && !festivalManagement.findAllFestivals().isEmpty()) {
+				inventory.save(new FoodInventoryItem(food, Quantity.of(5), festivalManagement.findAllFestivals()
+						.toList().get(0)));
 			}
 		});
 	}
