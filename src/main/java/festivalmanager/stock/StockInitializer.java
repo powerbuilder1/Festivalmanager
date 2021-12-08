3658327c6e60e60f45d100c7502fae5ddd4cef7b
powerbuilder1 <philipp.120201@gmail.com>
2021-12-08 17:11:36 +0100

    update StockInitializer: save FoodInventoryItems now with festival from food Item

--- src/main/java/festivalmanager/stock/StockInitializer.java
+++ src/main/java/festivalmanager/stock/StockInitializer.java
@@ -2,8 +2,6 @@ package festivalmanager.stock;
 
 import festivalmanager.festival.FestivalManagement;
 import org.salespointframework.core.DataInitializer;
-import org.salespointframework.inventory.UniqueInventory;
-import org.salespointframework.inventory.UniqueInventoryItem;
 import org.salespointframework.quantity.Quantity;
 import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
@@ -14,11 +12,11 @@ import festivalmanager.catering.FoodCatalog;
 @Order(30)
 public class StockInitializer implements DataInitializer {
 
-	private final UniqueInventory<UniqueInventoryItem> inventory;
+	private final StockInventory inventory;
 	private final FoodCatalog foodCatalog;
 	private final FestivalManagement festivalManagement;
 
-	public StockInitializer(UniqueInventory<UniqueInventoryItem> inventory, FoodCatalog foodCatalog, FestivalManagement festivalManagement) {
+	public StockInitializer(StockInventory inventory, FoodCatalog foodCatalog, FestivalManagement festivalManagement) {
 		this.inventory = inventory;
 		this.foodCatalog = foodCatalog;
 		this.festivalManagement = festivalManagement;
@@ -28,8 +26,7 @@ public class StockInitializer implements DataInitializer {
 	public void initialize() {
 		foodCatalog.findAll().forEach(food -> {
 			if (inventory.findByProduct(food).isEmpty() && !festivalManagement.findAllFestivals().isEmpty()) {
-				inventory.save(new FoodInventoryItem(food, Quantity.of(5), festivalManagement.findAllFestivals()
-						.toList().get(0)));
+				inventory.save(new FoodInventoryItem(food, Quantity.of(5), food.getFestival()));
 			}
 		});
 	}
