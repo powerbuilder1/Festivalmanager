e3643af93872217a253dfc39380d68ae6f16aa87
Conrad Carl <coheca0815@gmail.com>
2021-12-08 01:41:10 +0100

    added google maps map

--- src/main/resources/static/resources/js/location_locationmap.js
+++ src/main/resources/static/resources/js/location_locationmap.js
@@ -1,1 +1,18 @@
 const self = {};
+
+function initMap() {
+    console.log("initMap");
+    var map = new google.maps.Map(document.getElementById("map"), {
+        zoom: 15,
+        center: {
+            lat: 51.038200,
+            lng: 13.763133
+        },
+        mapTypeId: "satellite",
+    });
+    self.map = map;
+
+    google.maps.event.addListener(self.map, "click", (event) => {
+        create_polygon(event.latLng);
+    });
+}
