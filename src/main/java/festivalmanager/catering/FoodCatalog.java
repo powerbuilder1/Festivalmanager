3feaedb0149e49dde6a2fe63098c02be1e6b85f2
powerbuilder1 <philipp.120201@gmail.com>
2021-12-08 15:43:57 +0100

    implement findFoodByFestival method in FoodCatalog

--- src/main/java/festivalmanager/catering/FoodCatalog.java
+++ src/main/java/festivalmanager/catering/FoodCatalog.java
@@ -1,6 +1,9 @@
 package festivalmanager.catering;
 
+import festivalmanager.festival.Festival;
 import org.salespointframework.catalog.Catalog;
+import org.springframework.data.util.Streamable;
 
 public interface FoodCatalog extends Catalog<Food> {
+	Streamable<Food> findFoodsByFestival(Festival festival);
 }
