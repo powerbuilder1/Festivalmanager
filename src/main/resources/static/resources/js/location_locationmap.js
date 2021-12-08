7f1fdc01b967a8d3d4b664dc94d8491510e45b24
Conrad Carl <coheca0815@gmail.com>
2021-12-08 01:57:28 +0100

    locationmap: added randomColor for polygons

--- src/main/resources/static/resources/js/location_locationmap.js
+++ src/main/resources/static/resources/js/location_locationmap.js
@@ -88,6 +88,14 @@ const self = {
             return poly.id == id;
         })[0];
     },
+    randomColor: function() {
+        var letters = '0123456789ABCDEF';
+        var color = '#';
+        for (var i = 0; i < 6; i++) {
+            color += letters[Math.floor(Math.random() * 16)];
+        }
+        return color;
+    },
 };
 
 function initMap() {
