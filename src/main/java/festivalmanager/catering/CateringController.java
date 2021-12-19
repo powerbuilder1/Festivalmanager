fb05fc20d75bf57c361141c32fc50df8c9b95918
powerbuilder <philipp.120201@gmail.com>
2021-12-19 18:04:50 +0100

    change Errors to BindingResult in CateringController

--- src/main/java/festivalmanager/catering/CateringController.java
+++ src/main/java/festivalmanager/catering/CateringController.java
@@ -6,13 +6,9 @@ import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.validation.Errors;
-import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import javax.validation.Valid;
 import java.util.Optional;
@@ -64,13 +60,13 @@ public class CateringController {
 	public String addItemToCatalog(
 			@LoggedIn Optional<UserAccount> account,
 			@Valid NewFoodItemForm form,
-			Errors result
+			BindingResult result
 	) {
 		if (result.hasErrors()) {
 			return "catering_management";
 		}
 		cateringManagement.addItemToCatalog(form, account);
-		return "redirect:/";
+		return "redirect:/catering/catalog";
 	}
 
 	// delete item from FoodCatalog
@@ -87,7 +83,7 @@ public class CateringController {
 	public String editItemFromCatalog(
 			@PathVariable("foodItem") Food foodItem,
 			@Valid NewFoodItemForm form,
-			Errors result
+			BindingResult result
 	) {
 		if (result.hasErrors()) {
 			return "redirect:/catering/catalog/edit/" + foodItem.getId();
