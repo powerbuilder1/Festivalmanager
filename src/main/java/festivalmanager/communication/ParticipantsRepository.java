6411a6dce53a50d5f97947a91b012d54242a2cc2
Conrad Heinrich Carl <coheca0815@gmail.com>
2022-01-12 20:32:35 +0100

    javadoc: ParticipantsRepository

--- src/main/java/festivalmanager/communication/ParticipantsRepository.java
+++ src/main/java/festivalmanager/communication/ParticipantsRepository.java
@@ -3,6 +3,10 @@ package festivalmanager.communication;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.util.Streamable;
 
+/**
+ * @author Conrad
+ * ParticipantsRepository is used for storing the participants of the communication package
+ */
 public interface ParticipantsRepository extends CrudRepository<Participants, Long> {
 
     @Override
