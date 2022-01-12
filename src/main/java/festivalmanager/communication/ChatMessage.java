6e05f450e214ac8b7a9634cecf19bf0d18e33f4b
Conrad Heinrich Carl <coheca0815@gmail.com>
2022-01-12 19:46:37 +0100

    javadoc: ChatMessage

--- src/main/java/festivalmanager/communication/ChatMessage.java
+++ src/main/java/festivalmanager/communication/ChatMessage.java
@@ -11,30 +11,55 @@ import javax.persistence.ManyToOne;
 import festivalmanager.authentication.User;
 
 /**
+ * @author Conrad
  * ChatMessage class
  * used to store a message in the database
- * 
  */
 @Entity
 public class ChatMessage {
 
+    /**
+     * id of the message {@link Long}
+     */
     private @Id @GeneratedValue Long id;
 
+    /**
+     * the message {@link String}
+     */
     @Column(columnDefinition = "TEXT")
     private String message;
 
+    /**
+     * the sender of the message {@link User}
+     */
     @ManyToOne
     private User sender;
 
+    /**
+     * the room the message is in {@link Room}
+     */
     @ManyToOne
     private Room room;
 
+    /**
+     * the date the message was sent {@link Date}
+     */
     private Date timeStamp;
 
+    /**
+     * default constructor
+     */
     public ChatMessage() {
         // default constructor
     }
 
+    /**
+     * constructor
+     * @param message the message {@link String}
+     * @param sender the sender {@link User}
+     * @param room the room {@link Room}
+     * the date the message was sent is auto-generated
+     */
     public ChatMessage(String message, User sender, Room room) {
         setMessage(message);
         setSender(sender);
@@ -42,46 +67,91 @@ public class ChatMessage {
         setTimeStamp(new Date());
     }
 
+    /**
+     * getter
+     * @return the id {@link Long}
+     */
     public Long getId() {
         return this.id;
     }
 
+    /**
+     * setter
+     * @param id the id {@link Long}
+     */
     public void setId(Long id) {
         this.id = id;
     }
 
+    /**
+     * getter
+     * @return the message {@link String}
+     */
     public String getMessage() {
         return this.message;
     }
 
+    /**
+     * setter
+     * @param message the message {@link String}
+     */
     public void setMessage(String message) {
         this.message = message;
     }
 
+    /**
+     * getter
+     * @return the sender {@link User}
+     */
     public User getSender() {
         return this.sender;
     }
 
+    /**
+     * setter
+     * @param sender the sender {@link User}
+     */
     public void setSender(User sender) {
         this.sender = sender;
     }
 
+    /**
+     * getter
+     * @return the room {@link Room}
+     */
     public Room getRoom() {
         return this.room;
     }
 
+    /**
+     * setter
+     * @param room the room {@link Room}
+     */
     public void setRoom(Room room) {
         this.room = room;
     }
 
+    /**
+     * getter
+     * @return the date the message was sent {@link Date}
+     */
     public void setTimeStamp(Date date) {
         this.timeStamp = date;
     }
 
+    /**
+     * getter
+     * @return the date the message was sent {@link Date}
+     */
     public Date getTimeStamp() {
         return this.timeStamp;
     }
 
+    /**
+     * toString method
+     * @return {@link String}
+     * representation of the ChatMessage
+     */
     public String toString() {
         return "{" +
                 " id=" + this.id +
