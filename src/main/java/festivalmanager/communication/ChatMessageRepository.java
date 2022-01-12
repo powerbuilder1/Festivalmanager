8f60ead26a43c55245e8f4517f6b52f8667589fe
Conrad Heinrich Carl <coheca0815@gmail.com>
2022-01-12 19:48:48 +0100

    javadoc: ChatMessageRepository

--- src/main/java/festivalmanager/communication/ChatMessageRepository.java
+++ src/main/java/festivalmanager/communication/ChatMessageRepository.java
@@ -3,6 +3,11 @@ package festivalmanager.communication;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.util.Streamable;
 
+/**
+ * @author Conrad
+ * ChatMessageRepository class
+ * used to store a message in the database
+ */
 public interface ChatMessageRepository extends CrudRepository<ChatMessage, Long> {
 
     @Override
