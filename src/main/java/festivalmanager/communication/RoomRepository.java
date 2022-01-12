e71339feffbbb84738e48f67ec4835ba0ceda956
Conrad Heinrich Carl <coheca0815@gmail.com>
2022-01-12 20:35:32 +0100

    javadoc: RoomRepository

--- src/main/java/festivalmanager/communication/RoomRepository.java
+++ src/main/java/festivalmanager/communication/RoomRepository.java
@@ -3,6 +3,10 @@ package festivalmanager.communication;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.util.Streamable;
 
+/**
+ * @author Conrad
+ * RoomRepository is used for storing the rooms of the communication package
+ */
 public interface RoomRepository extends CrudRepository<Room, Long> {
 
     @Override
