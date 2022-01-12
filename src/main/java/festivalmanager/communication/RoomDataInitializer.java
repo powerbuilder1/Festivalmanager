2b01e80459d0c80f0726a383bc5d4b5b3b667537
Conrad Heinrich Carl <coheca0815@gmail.com>
2022-01-12 20:34:53 +0100

    javadoc: RoomDataInitializer

--- src/main/java/festivalmanager/communication/RoomDataInitializer.java
+++ src/main/java/festivalmanager/communication/RoomDataInitializer.java
@@ -7,17 +7,29 @@ import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 import org.springframework.util.Assert;
 
+/**
+ * @author Conrad
+ * RoomDataInitializer is a class that adds dummy rooms
+ */
 @Order(50)
 @Component
 public class RoomDataInitializer implements DataInitializer {
 
     private final CommunicationManagement communicationManagement;
 
+    /**
+     * constructor
+     * @param communicationManagement {@link CommunicationManagement}
+     */
     public RoomDataInitializer(CommunicationManagement communicationManagement) {
         Assert.notNull(communicationManagement, "communicationManagement must not be null!");
         this.communicationManagement = communicationManagement;
     }
 
+    /**
+     * @see org.salespointframework.core.DataInitializer#initialize()
+     * initialize the dummy rooms
+     */
     @Override
     public void initialize() {
         List.of("Catering", "public", "Planning").forEach(communicationManagement::createRoom);
