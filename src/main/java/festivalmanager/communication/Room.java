af1c1921a847c0500c4bcf5d6a4373376fc9162f
Conrad Heinrich Carl <coheca0815@gmail.com>
2022-01-12 20:33:52 +0100

    javadoc: room

--- src/main/java/festivalmanager/communication/Room.java
+++ src/main/java/festivalmanager/communication/Room.java
@@ -8,39 +8,72 @@ import javax.persistence.Id;
 import javax.persistence.OneToMany;
 
 /**
+ * @author Conrad
  * Room class
  * used to connect users to a room via Participants
  */
 @Entity
 public class Room {
 
+    /**
+     * id of the room (primary key) {@ling Long}
+     */
     private @Id @GeneratedValue Long id;
 
+    /**
+     * name of the room {@link String}
+     */
     private String name;
 
+    /**
+     * participants of the room {@link Set<Participants>}
+     */
     @OneToMany(mappedBy = "room")
     Set<Participants> participants;
 
+    /**
+     * default constructor
+     */
     public Room() {
         // default constructor
     }
 
+    /**
+     * constructor
+     * @param name the name {@link String}
+     */
     public Room(String name) {
         setName(name);
     }
 
+    /**
+     * getter
+     * @return the id {@link Long}
+     */
     public String getName() {
         return name;
     }
 
+    /**
+     * setter
+     * @param name the name {@link String}
+     */
     public void setName(String name) {
         this.name = name;
     }
 
+    /**
+     * getter
+     * @return the id {@link Long}
+     */
     public Long getId() {
         return this.id;
     }
 
+    /**
+     * setter
+     * @param id the id {@link Long}
+     */
     public void setId(Long id) {
         this.id = id;
     }
