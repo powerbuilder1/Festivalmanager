85129fc4d9414bd53f511b3cc5fba0cccc6b8fcc
Conrad Heinrich Carl <coheca0815@gmail.com>
2022-01-20 15:24:03 +0100

    added finance data classes

--- src/main/java/festivalmanager/finance/FinanceRepository.java
+++ src/main/java/festivalmanager/finance/FinanceRepository.java
@@ -1,5 +1,16 @@
 package festivalmanager.finance;
 
-public interface FinanceRepository {
-    
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.util.Streamable;
+
+
+/**
+ * @author Conrad
+ * stores Finance objects in a database
+ */
+public interface FinanceRepository extends CrudRepository<Finance, Long> {
+
+    @Override
+    Streamable<Finance> findAll();
+
 }
