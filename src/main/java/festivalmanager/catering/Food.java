4443d80cd632e01c0a091fa85d3294809c48c063
powerbuilder1 <philipp.120201@gmail.com>
2022-01-24 17:26:38 +0100

    add javadoc in Food

--- src/main/java/festivalmanager/catering/Food.java
+++ src/main/java/festivalmanager/catering/Food.java
@@ -15,19 +15,32 @@ public class Food extends Product {
 	@JoinColumn(name = "festival_id", nullable = false)
 	private Festival festival;
 
+	/**
+	 *
+	 * @param name
+	 * @param price
+	 * @param festival
+	 */
 	public Food(String name, Money price, Festival festival) {
 		super(name, price);
 		this.festival = festival;
 	}
 
 	public Food() {
-
 	}
 
+	/**
+	 * getter for festival
+	 * @return
+	 */
 	public Festival getFestival() {
 		return festival;
 	}
 
+	/**
+	 * setter for festival
+	 * @param festival
+	 */
 	public void setFestival(Festival festival) {
 		this.festival = festival;
 	}
