3392f61d2400ef490a09f7159fefa316febb71e5
powerbuilder1 <philipp.120201@gmail.com>
2022-01-24 17:24:06 +0100

    add javadoc in CateringManagement

--- src/main/java/festivalmanager/catering/CateringManagement.java
+++ src/main/java/festivalmanager/catering/CateringManagement.java
@@ -6,10 +6,8 @@ import festivalmanager.festival.FestivalManagement;
 import festivalmanager.stock.FoodInventoryItem;
 import festivalmanager.stock.StockManagment;
 import org.javamoney.moneta.Money;
-import org.salespointframework.catalog.Product;
 import org.salespointframework.catalog.ProductIdentifier;
 import org.salespointframework.core.Currencies;
-import org.salespointframework.inventory.UniqueInventoryItem;
 import org.salespointframework.useraccount.UserAccount;
 import org.springframework.data.util.Streamable;
 import org.springframework.stereotype.Service;
@@ -24,6 +22,12 @@ public class CateringManagement {
 	private FestivalManagement festivalManagement;
 	private final UserManagement userManagement;
 
+	/**
+	 *
+	 * @param foodCatalog
+	 * @param stockManagment
+	 * @param userManagement
+	 */
 	public CateringManagement(FoodCatalog foodCatalog, StockManagment stockManagment, UserManagement userManagement) {
 		this.foodCatalog = foodCatalog;
 		this.stockManagment = stockManagment;
@@ -31,11 +35,19 @@ public class CateringManagement {
 		this.festivalManagement = null;
 	}
 
+	/**
+	 * setter for festivalManagement
+	 * @param festivalManagement
+	 */
 	public void setFestivalManagement(FestivalManagement festivalManagement) {
 		this.festivalManagement = festivalManagement;
 	}
 
-	// add item to Catalog
+	/**
+	 * add item to Catalog
+	 * @param foodItemForm
+	 * @param userAccount
+	 */
 	public void addItemToCatalog(NewFoodItemForm foodItemForm, Optional<UserAccount> userAccount) {
 		userAccount.ifPresent(account -> {
 			Festival festival = userManagement.findUserByUserAccount(userAccount.get()).getFestival();
@@ -45,10 +57,13 @@ public class CateringManagement {
 					festival));
 			stockManagment.initializeInventoryItem(foodItem, 0, festival);
 		});
-		// get festival from user over userAccount
 	}
 
-	// get catalog
+	/**
+	 * get catalog by userAccount
+	 * @param userAccount
+	 * @return
+	 */
 	public Streamable<Food> getCatalog(Optional<UserAccount> userAccount) {
 		if (userAccount.isPresent()) {
 			Festival festival = userManagement.findUserByUserAccount(userAccount.get()).getFestival();
@@ -57,11 +72,20 @@ public class CateringManagement {
 		return null;
 	}
 
+	/**
+	 * get catalog by festivalId
+	 * @param festivalId
+	 * @return
+	 */
 	public Streamable<Food> getCatalog(long festivalId) {
 		Festival festival = festivalManagement.findById(festivalId);
 		return foodCatalog.findFoodsByFestival(festival);
 	}
 
+	/**
+	 * delete foodItem from catalog
+	 * @param foodItem
+	 */
 	public void deleteItemFromCatalog(Food foodItem) {
 		if (stockManagment.findByProduct(foodItem).isPresent()) {
 			FoodInventoryItem inventoryItem = stockManagment.findByProduct(foodItem).get();
@@ -70,7 +94,11 @@ public class CateringManagement {
 		}
 	}
 
-	// update item form FoodCatalog
+	/**
+	 * update item from FoodCatalog
+	 * @param foodItem
+	 * @param foodItemForm
+	 */
 	public void editItemFromCatalog(Food foodItem, NewFoodItemForm foodItemForm) {
 		if (foodItem.getId() != null) {
 			foodItem.setName(foodItemForm.getName());
@@ -79,6 +107,11 @@ public class CateringManagement {
 		}
 	}
 
+	/**
+	 * find food product by ProductIdentifier
+	 * @param productIdentifier
+	 * @return
+	 */
 	public Optional<Food> findProductById(ProductIdentifier productIdentifier) {
 		return foodCatalog.findById(productIdentifier);
 	}
