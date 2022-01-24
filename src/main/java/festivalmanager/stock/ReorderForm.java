bc1216a2b7d70d0a93588ac2612e1f880b500f4d
powerbuilder1 <philipp.120201@gmail.com>
2022-01-24 17:27:40 +0100

    add javadoc in ReorderForm

--- src/main/java/festivalmanager/stock/ReorderForm.java
+++ src/main/java/festivalmanager/stock/ReorderForm.java
@@ -9,6 +9,11 @@ public class ReorderForm {
 	private @NotNull ProductIdentifier foodItemId;
 	private @NotNull @Positive int amount;
 
+	/**
+	 *
+	 * @param foodItemId
+	 * @param amount
+	 */
 	public ReorderForm(ProductIdentifier foodItemId, int amount) {
 		this.foodItemId = foodItemId;
 		this.amount = amount;
@@ -17,20 +22,36 @@ public class ReorderForm {
 	public ReorderForm() {
 	}
 
+	/**
+	 * getter for foodItem ProductIdentifier
+	 * @return
+	 */
 	public ProductIdentifier getFoodItemId() {
 		return foodItemId;
 	}
 
+
+	/**
+	 * setter for foodItem ProductIdentifier
+	 * @param foodItemId
+	 */
 	public void setFoodItemId(ProductIdentifier foodItemId) {
 		this.foodItemId = foodItemId;
 	}
 
+	/**
+	 * getter for amount
+	 * @return
+	 */
 	public int getAmount() {
 		return amount;
 	}
 
+	/**
+	 * setter for amount
+	 * @param amount
+	 */
 	public void setAmount(int amount) {
 		this.amount = amount;
 	}
-
 }
