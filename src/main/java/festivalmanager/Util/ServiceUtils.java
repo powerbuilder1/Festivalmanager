c59198e6d4e5914876b9cc22779bf85df23d9f15
Conrad Heinrich Carl <coheca0815@gmail.com>
2022-01-25 19:25:27 +0100

    serviceutils: javadoc

--- src/main/java/festivalmanager/Util/ServiceUtils.java
+++ src/main/java/festivalmanager/Util/ServiceUtils.java
@@ -8,6 +8,10 @@ import org.springframework.stereotype.Component;
 import festivalmanager.festival.FestivalManagement;
 import festivalmanager.location.LocationManagement;
 
+/**
+ * @author Conrad
+ * Singleton class for accessing managements inside the validator
+ */
 @Component
 public class ServiceUtils {
 
@@ -18,15 +22,26 @@ public class ServiceUtils {
     @Autowired
     private FestivalManagement festivalManagement;
 
+    /**
+     * Singleton "constructor"
+     */
     @PostConstruct
     public void fillInstance() {
         instance = this;
     }
 
+    /**
+     * Returns the LocationManagement
+     * @return {@link LocationManagement}
+     */
     public static LocationManagement getLocationManagement() {
         return instance.locationManagement;
     }
 
+    /**
+     * Returns the FestivalManagement
+     * @return {@link FestivalManagement}
+     */
     public static FestivalManagement getFestivalManagement() {
         return instance.festivalManagement;
     }
