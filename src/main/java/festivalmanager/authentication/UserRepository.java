6031369f981e63d967e6f98be071b78bb3742d55
powerbuilder1 <philipp.120201@gmail.com>
2022-01-25 19:23:17 +0100

    add deleteUsersByFestival_id, deleteUsersByName methods to UserRepository

--- src/main/java/festivalmanager/authentication/UserRepository.java
+++ src/main/java/festivalmanager/authentication/UserRepository.java
@@ -4,6 +4,8 @@ import org.salespointframework.useraccount.UserAccount;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.util.Streamable;
 
+import java.util.Optional;
+
 /**
  * A repository interface to manage {@link User} instances.
  */
@@ -16,6 +18,30 @@ public interface UserRepository extends CrudRepository<User, Long> {
 	@Override
 	Streamable<User> findAll();
 
+	/**
+	 * find user by UserAccount
+	 * @param userAccount
+	 * @return
+	 */
 	User findUserByUserAccount(UserAccount userAccount);
+
+	/**
+	 * delete users by festivalId
+	 * @param festivalId
+	 */
+	void deleteUsersByFestival_Id(Long festivalId);
+
+	/**
+	 * delete user by name
+	 * @param name
+	 */
+	void deleteUsersByName(String name);
+
+	/**
+	 * find user by festivalId
+	 * @param festivalId
+	 * @return
+	 */
+	Optional<User> findUsersByFestival_Id(Long festivalId);
 
 }
